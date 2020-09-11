778b01a143ac8204d68958c5c02e82f2794fe098
umutkiziloglu <umutkiziloglu00@gmail.com>
2020-09-11 13:48:25 +0300

    update

--- app.js
+++ app.js
@@ -5,13 +5,29 @@ class oxygenAPI {
  
   this.key = key;  
     
-  if(!this.key) throw new Error('[API] Key Girilmemiş!');
+  if(!this.key) throw new Error('Key Girilmemiş!');
  }
   
   async saat() {
   const { body } = await request.get("https://api.oxygenbot.cf/api/saat?key="+ this.key);
-  if (body.error) throw new Error('[API] Bir Problem Çıktı : ' + body.error);  
+  if (body.error) throw new Error('Bir Problem Çıktı : ' + body.error);  
   return body;
   }
+  async steam(q) {
+    const { body } = await request.get("https://api.oxygenbot.cf/api/steam?q="+ q +"&key="+ this.key);
+    if (body.error) throw new Error('Bir Problem Çıktı : ' + body.error);  
+    return body;
+    }
+    async steam(q) {
+      const { body } = await request.get("https://api.oxygenbot.cf/api/steam?q="+ q +"&key="+ this.key);
+      if (body.error) throw new Error('Bir Problem Çıktı : ' + body.error);  
+      return body;
+      }
+      async sifre(lenght) {
+        if (!lenght) throw new Error("Şifre oluşturucu genişliği girilmedi!")
+        const { body } = await request.get("https://api.oxygenbot.cf/api/sifre"+ lenght +"?key="+ this.token);
+          if (body.error) throw new Error('Bir sorun oluştu;\n'+ body.error); 
+        return body.cikti
+     }
 }
 module.exports = oxygenAPI;
